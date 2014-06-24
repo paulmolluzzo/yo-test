6e2fc54403692d6bdacc1764a8ce7b327bbaafd2
Paul Molluzzo <paul@molluzzo.com>
2014-06-24 15:03:36 -0400

    Working version

--- client/client.js
+++ client/client.js
@@ -1,12 +1,20 @@
 Template.home.events({
     'click #yo': function(){
         var yoKey = document.getElementById('api-key').value;
-        Meteor.call('yoAll', yoKey, function(err, result){
-            if(!err){
-                console.log(result);
-            } else {
-                console.error(err)
-            }
-        });
+        if (yoKey.length > 1) {
+            Meteor.call('yoAll', yoKey, function(err, result){
+                if(!err){
+                    if (result.data.result == "OK") {
+                        $('#yo').html('Yo Sent!')
+                    } else {
+                        alert('Something went wrong. Try again.')
+                    }
+                } else {
+                    alert('Something went wrong. Try again.')
+                }
+            });
+        } else {
+            alert('You need to enter an API Key');
+        }
     }
 })
